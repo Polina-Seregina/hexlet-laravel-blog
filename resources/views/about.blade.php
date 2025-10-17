4b2ffa1dd3c8aaa56cade756719dd19e57ed3acc
Polina Seregina <p.seregina2015@yandex.ru>
2025-10-17 21:32:54 +0300

    with use Blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'О блоге')
+
+@section('content')
 <h1> О блоге </h1>
-<p> Эксперименты с Laravel на Хэкслете </p>
+<p> Эксперименты с Laravel на Хекслете </p>
+@endsection
+
+
